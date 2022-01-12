608dbf73a42cdfa500a36253bd9f325accafe75f
BMH <minhobaek@Mino-MacBook-Pro2017.local>
2022-01-12 09:12:37 +0900

    2-5 게시글 작성/삭제 더미 데이터

--- ch1/front/components/PostCard.vue
+++ ch1/front/components/PostCard.vue
@@ -4,8 +4,8 @@
       <v-img />
       <v-card-text>
         <div>
-          <h3>ZeroCho</h3>
-          <div>게시글1</div>
+          <h3>{{post.User.nickname}}</h3>
+          <div>{{post.content}}</div>
         </div>
       </v-card-text>
       <v-card-actions>
@@ -18,9 +18,17 @@
         <v-btn text color="orange">
           <v-icon>mdi-comment-outline</v-icon>
         </v-btn>
-        <v-btn text color="orange">
-          <v-icon>mdi-dots-horizontal</v-icon>
-        </v-btn>
+        <v-menu offset-y open-on-hover>
+          <template v-slot:activator="{ on }">
+            <v-btn text color="orange" v-on="on">
+              <v-icon>mdi-dots-horizontal</v-icon>
+            </v-btn>
+          </template>
+          <div style="background: white">
+            <v-btn dark color="red" @click="onRemovePost">삭제</v-btn>
+            <v-btn text color="orange" @click="onEditPost">수정</v-btn>
+          </div>
+        </v-menu>
       </v-card-actions>
     </v-card>
   </v-container>
@@ -28,7 +36,22 @@
 
 <script>
 export default {
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    onRemovePost() {
+      this.$store.dispatch('posts/remove', {
+        id: this.post.id,
+      })
+    },
+    onEditPost() {
 
+    }
+  }
 }
 </script>
 
--- ch1/front/components/PostForm.vue
+++ ch1/front/components/PostForm.vue
@@ -1,25 +1,26 @@
 <template>
-  <v-card>
+  <v-card style="margin-bottom: 20px">
     <v-container>
       <v-form ref="form" v-model="valid" @submit.prevent="onSubmitForm">
         <v-textarea
           v-model="content"
-          outline
+          outlined
           auto-grow
           clearable
-          label="쓸 내용은?"
+          label="어떤 신기한 일이 있었나요?"
           :hide-details="hideDetails"
-          :sucess-messages="successMessages"
+          :success-messages="successMessages"
           :success="success"
-          :rules="[v => !!v.trim() || '내용을 입력하세요']"
+          :rules="[v => !!v.trim() || '내용을 입력하세요.']"
           @input="onChangeTextarea"
         />
+        <v-btn type="submit" color="green" absolute right>확인</v-btn>
+        <v-btn>이미지 업로드</v-btn>
       </v-form>
-      <v-btn type="submit" color="green" absolute right>확인</v-btn>
-      <v-btn>이미지 업로드</v-btn>
     </v-container>
   </v-card>
 </template>
+
 
 <script>
 import { mapState } from 'vuex';
@@ -38,24 +39,27 @@ export default {
     ...mapState('users', ['me']),
   },
   methods: {
-    onChangeTextarea() {
-      this.hideDetails = true;
-      this.success = false;
-      this.successMessages = '';
+    onChangeTextarea(value) {
+      if(value.length) {
+        this.hideDetails = true;
+        this.success = false;
+        this.successMessages = '';
+      }
     },
     onSubmitForm() {
-      if(this.$refs.form.validata()){
-        this.$store.dispatch('post/add', {
+      if(this.$refs.form.validate()){
+        this.$store.dispatch('posts/add', {
           content: this.content,
           User: {
-
+            nickname: this.me.nickname,
           },
           Comments: [],
           Images: [],
-          id: Data.now(),
-          createAt: Date.now(),
+          id: Date.now(),
+          createdAt: Date.now(),
         })
         .then(() => {
+          this.content = '';
           this.hideDetails = false;
           this.success = true;
           this.successMessages = '게시글 등록 성공';
